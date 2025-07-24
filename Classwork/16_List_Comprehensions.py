f16779cd9e30be866ba39f7f9254a91dce18fb39
gopichand-duriseti <gopiduriseti@gmail.com>
2025-07-24 10:22:06 +0530

    List Comprehensions

--- Classwork/16_List_Comprehensions.py
+++ Classwork/16_List_Comprehensions.py
@@ -12,11 +12,9 @@ names = ["alice", "bob", "charlie"]
 upper_names = [name.upper() for name in names]
 print(upper_names)
 
-
 data=['pen','Book']
 dic1={i+1:data[i] for i in range(len(data))}
 print(dic1)
-
 
 t=tuple(i for i in range(1,10))
 print(t)
