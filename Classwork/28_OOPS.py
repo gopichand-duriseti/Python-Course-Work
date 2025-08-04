ba9eb10bd05e712d4fc8796439cf63a6a853fef7
gopichand-duriseti <gopiduriseti@gmail.com>
2025-08-04 10:15:25 +0530

    OOPS

---
+++ Classwork/28_OOPS.py
@@ -0,0 +1,30 @@
+print("OBJECT ORIENTED PROGRAMMING LANGUAGE".center(100,'-'))
+'''
+class Insta2010:
+    followers=set()
+    addfollowers()
+    removefollowers()
+    following={}
+    addfollowing()
+    unfollow()
+    username=''
+    bio=''
+    post=[]
+    closefriends=[]
+    highlights=[]
+    block()
+    unblock()
+    blockpeople={}
+    share()
+    sendrequest()
+    reels()
+    sendmessage()
+
+gopi=Insta()
+gopi.share()
+
+Sanjay=Insta()
+Sanjay.bio='hii peoooopleeeeee'
+
+'''
+
