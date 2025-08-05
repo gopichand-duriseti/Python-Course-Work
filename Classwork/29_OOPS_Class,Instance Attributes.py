9606ea84c7577cf4f2a1a0cba5cb6244543919ef
gopichand-duriseti <gopiduriseti@gmail.com>
2025-08-05 09:57:39 +0530

    OOPS

--- Classwork/29_OOPS_Class,Instance Attributes.py
+++ Classwork/29_OOPS_Class,Instance Attributes.py
@@ -42,9 +42,55 @@ class shopping:
     def product(self,price,name):
         self.price=price
         self.name=name
-        return self.price,self.name #price,name are called instance attributes(vary from one to another)
+        return self.price,self.name #price,name are called instance attributes(vary from user to user)
 user1=shopping()
 user2=shopping()
 print(user1.product(3400,'laptop'))
 print(shopping.discount) #10
 print(user1.price) #3400
+user1.price=4000
+shopping.discount=13 
+
+#METHODS - CLASS,INSTANCE AND STATIC METHOD
+
+#Class Method
+class shopping:
+    discount=10 #The attribute that is defined inside class is called class attribute
+
+    @classmethod
+    def update(cls,new_discount):
+        cls.discount=new_discount
+        print(f'{new_discount} is updated')
+    def product(self,price,name):
+        self.price=price
+        self.name=name
+        return self.price,self.name
+user1=shopping()
+shopping.update(15)
+
+#Static Method (used for calculations)
+class shopping:
+    products=[]
+    discount=10 #The attribute that is defined inside class is called class attribute
+
+    @classmethod
+    def update(cls,new_discount):
+        cls.discount=new_discount
+        print(f'{new_discount} is updated')
+    def product(self,price,name):
+        self.price=price
+        self.name=name
+        return self.price,self.name
+    @staticmethod
+    def check(products,price):
+        if products:
+            print(f"Go and shop,Items available:-{products}")
+        else:
+            print('Out of Stock')
+            return False
+        if price:
+            print("price:","$%.2f"%price,sep='-')
+        
+user1=shopping()
+user1.check([],0000)
+shopping.check(['laptop','mobile'],4000) #can access products and price using both class name and object name
