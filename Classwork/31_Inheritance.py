c870cdfa96d25293efdff43eeeb88bf979ad5ccf
gopichand-duriseti <gopiduriseti@gmail.com>
2025-08-07 09:32:35 +0530

    OOPS_Inheritance

--- Classwork/31_Inheritance.py
+++ Classwork/31_Inheritance.py
@@ -1,4 +1,4 @@
-class Status:
+'''class Status:
     def _init_(self,caption,image=None,video=None):
         self.caption = caption
         self.image = image
@@ -16,12 +16,30 @@ class StatusV1(Status):
     def addmusic(self,music):
         print(f"{music} Added")
 
-gopal = StatusV1("Hey I'm using whatsapp","Goodmrng.png")
+gopal = Status("Hey I'm using whatsapp","Goodmrng.png")
 gopal.see_status()
 gopal.likes()
 gopal.addmusic("Fear song")
 
-arun = StatusV1("Good Evening","coffee.png")
+arun = Status("Good Evening","coffee.png")
 arun.see_status()
 arun.likes()
-arun.addmusic('My love is gone')
+arun.addmusic('My love is gone')'''
+
+
+#SINGLE INHERITANCE
+class Status:
+    def uploadImage(self,imageurl):
+        self.image=imageurl
+        print(f"{self.image} is uploaded to your status")
+class StatusV1(Status):
+    def addCaption(self,text=None):
+        self.caption=text
+        print(f'"{self.caption}" is addded to your status')
+gopi=Status()
+gopi.uploadImage('selfie.png')
+
+siddhu=StatusV1()
+siddhu.uploadImage('Good Morning.png')
+siddhu.addCaption('Hello Friends!!!!')
+
