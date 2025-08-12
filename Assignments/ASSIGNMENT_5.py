52fb8c6401ad9c682a79d9f724306091b86a0149
gopichand-duriseti <gopiduriseti@gmail.com>
2025-08-12 10:36:18 +0530

    Assignment-5

--- Assignments/ASSIGNMENT_5.py
+++ Assignments/ASSIGNMENT_5.py
@@ -1,9 +1,9 @@
 #TRAIN
-#service=online
-#ECTRAIN(TRAIN)
-#name,age,seats_available(attributes)---_name(protected),__seats_available(private),age(public)
+#service=online #class variable
+#ECTRAIN(TRAIN) #Inheritance
+#name,age,seats_available(attributes)---_name(protected),__seats_available(private),age(public) #encapsulation
 #Book_Ticket and Cancel_Ticket(Methods)---Instance method
 #display_details(self)---related to train
-#display_details(self)---related to ectrain
+#display_details(self)---related to ectrain #polymorphism
 #@classmethod #def servicing(cls):return cls.service---class method
 #@staticmethod #if __seats_available>0
