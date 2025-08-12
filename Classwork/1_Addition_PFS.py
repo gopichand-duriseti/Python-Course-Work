4156afe42428141afd375d7d830c6d026c4472f4
gopichand-duriseti <gopiduriseti@gmail.com>
2025-08-12 09:27:03 +0530

    1_Addition

--- Classwork/1_Addition_PFS.py
+++ Classwork/1_Addition_PFS.py
@@ -1,3 +1,12 @@
 a=10
 b=20
 print("Addition:",a+b) #Addition: 30
+class Art:
+    def __init__(self,n):
+        self.n=n
+    def __add__(self,other):
+        return Art(self.n + other.n)
+c=Art(5)
+c1=Art(10)
+c2=Art(20)
+print(c + c1 + c2)
