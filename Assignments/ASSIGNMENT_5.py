bea0f9e27977927e478dfa954142a33162817119
gopichand-duriseti <gopiduriseti@gmail.com>
2025-08-12 10:09:22 +0530

    Assignment-5

---
+++ Assignments/ASSIGNMENT_5.py
@@ -0,0 +1,9 @@
+#TRAIN
+#service=online
+#ECTRAIN(TRAIN)
+#name,age,seats_available(attributes)---_name(protected),__seats_available(private),age(public)
+#Book_Ticket and Cancel_Ticket(Methods)---Instance method
+#display_details(self)---related to train
+#display_details(self)---related to ectrain
+#@classmethod #def servicing(cls):return cls.service---class method
+#@staticmethod #if seats_available>0
