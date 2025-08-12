2620768ca84ccce38cf1bcf99f1970a9b2506b92
gopichand-duriseti <gopiduriseti@gmail.com>
2025-08-12 10:18:39 +0530

    Assignment-4

--- Assignments/Assignment4/gopi_main_4.py
+++ Assignments/Assignment4/gopi_main_4.py
@@ -17,43 +17,43 @@ while True:
         break
 
     elif ch == 1:
-        from my_programs_4 import reverse_wrd
+        from gopi_my_programs_4 import reverse_wrd
         reverse_wrd()
 
     elif ch == 2:
-        from my_programs_4 import emoji
+        from gopi_my_programs_4 import emoji
         emoji()
 
     elif ch == 3:
-        from my_programs_4 import dec_bin
+        from gopi_my_programs_4 import dec_bin
         dec_bin()
 
     elif ch == 4:
-        from my_programs_4 import date_diff
+        from gopi_my_programs_4 import date_diff
         date_diff()
 
     elif ch == 5:
-        from my_programs_4 import large_ele
+        from gopi_my_programs_4 import large_ele
         large_ele()
 
     elif ch == 6:
-        from my_programs_4 import count_matches
+        from gopi_my_programs_4 import count_matches
         count_matches()
 
     elif ch == 7:
-        from my_programs_4 import sqrs
+        from gopi_my_programs_4 import sqrs
         sqrs()
 
     elif ch == 8:
-        from my_programs_4 import bin_anagram
+        from gopi_my_programs_4 import bin_anagram
         bin_anagram()
 
     elif ch == 9:
-        from my_programs_4 import sort_tuple
+        from gopi_my_programs_4 import sort_tuple
         sort_tuple()
 
     elif ch == 10:
-        from my_programs_4 import pattern
+        from gopi_my_programs_4 import pattern
         pattern()
 
     else:
