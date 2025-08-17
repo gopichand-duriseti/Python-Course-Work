66189cf747a449a67919c4b9adfe77196c338277
gopichand-duriseti <gopiduriseti@gmail.com>
2025-08-17 14:34:15 +0530

    Assignment-5

--- Assignments/Assignment-5/ASSIGNMENT_5.py
+++ Assignments/Assignment-5/ASSIGNMENT_5.py
@@ -71,18 +71,4 @@ class PassengerDetails:
     def passenger_count(count_pas):
         return len(count_pas)
 
-#Cost should be checked by considering train and reservation choice
-t=Train("hii",134567,190,900)
-print(t.get_details())
-c=EVTrain("hello",123456,23,1000)
-print(c.get_details())
-print(c.service())
-p=PassengerDetails('gopi',23,'2s',t)
-print(p.book_tickets('Hyd-Vij'))
-print(p.book_tickets('Hy-Vij'))
-print(p.cancel_ticket('Hyd-Vij'))
-print(p.passenger_count(p.status.passengers_name))
-tot_amt=p.Tot_Ticket_Price_Booked(p.status.booked,167)
-tot_amt_canc=p.Tot_Ticket_Price_Booked(p.status.cancelled,167)
-print(f'Total amount after booking and cancellation:{tot_amt-tot_amt_canc}')
 
