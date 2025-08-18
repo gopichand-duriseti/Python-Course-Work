471ffc5004d579a5005fe90f52e81689d0590730
gopichand-duriseti <gopiduriseti@gmail.com>
2025-08-18 08:34:13 +0530

    Assignment-5

--- Assignments/Assignment-5/Exec.py
+++ Assignments/Assignment-5/Exec.py
@@ -53,7 +53,7 @@ if ch==1:
         p=PassengerDetails(name,age,reservation_choice,t)
         print(p.book_tickets(input("Enter your journey: ")))
 elif ch==2:
-    p=PassengerDetails(name,age,reservation_choice,t)
+    #p=PassengerDetails(name,age,reservation_choice,t)
     print(p.cancel_ticket(input('Enter the journey you booked: ')))
 
 '''c=EVTrain("hello",123456,23,1000)
