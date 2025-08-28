cab277b87c0d7026797f77f667283ef0b3bb0aba
gopichand-duriseti <gopiduriseti@gmail.com>
2025-08-28 09:35:10 +0530

    Email_Automation

--- Classwork/Email_Automation.py
+++ Classwork/Email_Automation.py
@@ -1,1 +1,27 @@
-print(1)
+#https://myaccount.google.com/apppasswords
+#App Name is Gopi App
+#password(9:25 AM):-scqd kgba wkds ayjs
+
+import smtplib #Simple mail transfer protocal for sending mails
+import os      # For File path and file existance check
+import csv     #To read email addresses from a CSV file
+
+#for creating multipart email messages
+from email.mime.multipart import MIMEMultipart
+
+#for adding plain text to email body
+from email.mime.text import MIMEText
+
+
+SMTP_SERVER="smtp.gmail.com"
+SMTP_PORT= 587 #Port Used for TLS(encryption)
+SENDER_EMAIL="gopiduriseti@gmail.com"
+SENDER_PASSWORD="scqd kgba wkds ayjs"
+
+def send_email(to_email,subject,body):
+    try:
+        msg=MIMEMultipart()
+        msg["From"]=SENDER_EMAIL
+        msg["To"]=to_email
+    except:
+        pass
